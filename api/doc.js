2f927ed38cf31d06e9abc1d35073e59b2f294eb8
mayankraval13 <iammayankraval@gmail.com>
2025-10-30 14:50:14 +0530

    third commit

--- api/doc.js
+++ api/doc.js
@@ -7,6 +7,12 @@ export default async function handler(req, res) {
     const response = await fetch(docUrl);
     const html = await response.text();
 
+    text = text.replace(/<div id="header"[\s\S]*?<\/div>/, ""); // common pattern
+    text = text.replace(
+      /Report abuse|Learn more|Updated automatically every 5 minutes/gi,
+      ""
+    );
+
     res.setHeader("Content-Type", "text/html");
     res.status(200).send(html);
   } catch (err) {
